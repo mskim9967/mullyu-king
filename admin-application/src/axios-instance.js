fe4077a1b7651eb1e2207725769f49892d9fb6af
mskim9967 <mskim9967@gmail.com>
2022-01-09 20:25:09 +0900

    update admin page

--- admin-application/src/axios-instance.js
+++ admin-application/src/axios-instance.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 export default axios.create({
-  baseURL: 'http://127.0.0.1:3000/api',
+  baseURL: process.env.REACT_APP_SERVER_BASEURL,
   headers: {
     post: {
       Authorization: localStorage.getItem('Authorization'),
